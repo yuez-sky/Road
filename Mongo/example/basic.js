7dae251fdbdf868d8ec23d505e8d4e60aafb6dae
zysndy <zysndy@gmail.com>
2018-10-18 23:43:30 +0800

    init(puppeteer)

--- Mongo/example/basic.js
+++ Mongo/example/basic.js
@@ -3,10 +3,15 @@ const mongoose = require('./connect')
 const Schema = mongoose.Schema
 // Schema
 const BasicSchema = new Schema({
-
+    name: String
 })
 
 // Model
 const Basic = mongoose.model('Basic', BasicSchema)
 
 // Document
+const document = new Basic({
+    name: '123'
+})
+
+document.save()
